37a47e8d990b85d5e508f052463268eff2ab136d
Rainux Luo <rainux@gmail.com>
2026-04-15 22:16:44 +0800

    Create blank AppKit app

---
+++ View/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  View
+//
+//  Created by Rainux Luo on 2026/4/15.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ View/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  View
+//
+//  Created by Rainux Luo on 2026/4/15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
