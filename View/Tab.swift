2a0192cfab5c11395d75288df7f6156e90b18457
Rainux Luo <rainux@gmail.com>
2026-04-16 05:11:23 +0800

    Add Tab model with optional webView for lazy state

---
+++ View/Tab.swift
@@ -0,0 +1,37 @@
+import AppKit
+import Foundation
+import WebKit
+
+final class Tab {
+    var persistenceID: Int64?
+    var url: URL
+    var title: String?
+    var webView: WKWebView?
+    var position: Int
+
+    var onTitleChange: ((Tab) -> Void)?
+    var onNavigationFinish: ((Tab) -> Void)?
+
+    var isLoaded: Bool { webView != nil }
+
+    init(
+        persistenceID: Int64? = nil,
+        url: URL,
+        title: String? = nil,
+        position: Int
+    ) {
+        self.persistenceID = persistenceID
+        self.url = url
+        self.title = title
+        self.webView = nil
+        self.position = position
+    }
+
+    func adopt(webView: WKWebView) {
+        self.webView = webView
+    }
+
+    func releaseWebView() {
+        self.webView = nil
+    }
+}
